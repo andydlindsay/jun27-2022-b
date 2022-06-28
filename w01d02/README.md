aa6520344c7746a555ca228a4f67001cea619431
Andy Lindsay <andydlindsay@gmail.com>
2022-06-28 12:10:32 -0700

    updates the readme file and adds the summer file

--- w01d02/README.md
+++ w01d02/README.md
@@ -1,19 +1,35 @@
 # W01D02 - The Dev Workflow
 
 ### To Do
-- [ ] Curriculum overview
+- [x] Curriculum overview
 - [ ] Example problem workflow
 - [ ] Problem solving tips
 - [ ] Review git workflow
 
+### Curriculum Overview
+
+* Week 1 && 2: FOCAL fundamentals (lotide)
+* Week 3 Back-end/server-side rendering
+* Week 4 Front-end/client-side rendering
+* Week 5 Database
+* Week 6 Midterm Project
+
+* Week 7 && 8: React
+* Week 9 && 10: Real-world simulator (Rails)
+* Week 11 && 12: Final Projects
+
+Fundamental Friday: compsci topics (oop, recursion, trees, adv data structures)
+Exam Friday: cover the prev week's topics
+
+Problem: Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
 
 
-
-
-
-
-
-
+```js
+BEDMAS
+PEDMAS
+is   (2 > 1 && !(3 > 2 && 4 > 1)) different 
+from (2 > 1 && 3 > 2 && 4 > 1)
+```
 
 
 
---
+++ w01d02/summer.js
@@ -0,0 +1,44 @@
+// takes in an unlimited number of command line args
+
+// console.log(process.argv);
+
+const arguments = process.argv;
+
+// arguments.shift();
+// arguments.shift();
+
+// arguments[0] = undefined;
+// arguments[1] = undefined;
+
+// delete arguments[0];
+
+// arguments.splice(0, 2);
+
+const sliced = arguments.slice(2);
+
+// console.log(sliced);
+// console.log(process.argv);
+
+const hardcodedArr = [ '5', '10', '15', '3.14', '-100' ];
+
+// prints out the sum of them
+
+// a variable to hold our running total
+let runningTotal = 0;
+
+// loop through the array: c-style, forEach, for..of, for..in
+for (let i = 0; i < hardcodedArr.length; i++) {
+  const element = hardcodedArr[i]; // '15'
+
+  // casting the element to a number
+  const number = Number(element); // 15
+
+  // ask if the number is a whole number and ask if it is above zero
+  if (Number.isInteger(number) && number > 0) {
+    // += each element of the array onto the running total
+    runningTotal += number;
+  }
+}
+
+console.log('runningTotal', runningTotal);
+// console.log(`runningTotal ${['a', 'b', 'c']}`);
